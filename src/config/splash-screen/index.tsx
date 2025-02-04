4ca2bdccd1061cc5b9347c3c54ed5f7a52261ba1
mohamad-ridwan <mr643062@gmail.com>
2025-02-04 16:03:44 +0700

    feat: add navigation and screens for Home and Login

--- src/config/splash-screen/index.tsx
+++ src/config/splash-screen/index.tsx
@@ -1,11 +1,15 @@
 import { useEffect } from "react";
 import { getAccessToken } from "../../services/storage-management/auth/getAccessToken";
 import BootSplash from "react-native-bootsplash";
+import { navigate } from "../../navigation/RootNavigation";
 
 export default function ConfigSplashScreen() {
     useEffect(() => {
         const init = async () => {
             const accessToken = await getAccessToken()
+            if(!accessToken){
+                navigate('Login')
+            }
         };
 
         init().finally(async () => {
---
+++ src/navigation/index.tsx
@@ -0,0 +1,23 @@
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/home-screen';
+import LoginScreen from '../screens/login-screen';
+import { navigationRef } from './RootNavigation';
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigation() {
+    return (
+        <NavigationContainer ref={navigationRef}>
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Home"
+                    component={HomeScreen}
+                    options={{ title: 'Welcome' }}
+                />
+                <Stack.Screen name="Login" component={LoginScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
